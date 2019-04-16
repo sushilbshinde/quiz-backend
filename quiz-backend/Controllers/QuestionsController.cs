b7269524be08f941e98e2427745b0ff6b05c06d1
Sushil <sushilbshinde@gmail.com>
2019-04-16 12:06:36 +0530

    Edit question route

--- quiz-backend/Controllers/QuestionsController.cs
+++ quiz-backend/Controllers/QuestionsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace quiz_backend.Controllers
 {
@@ -24,10 +25,25 @@ namespace quiz_backend.Controllers
         }
 
         [HttpPost]
-        public void Post([FromBody] Models.Question question)
+        public async Task<IActionResult> Post([FromBody] Models.Question question)
         {
             context.Questions.Add(question);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
+
+            return Ok(question);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Models.Question question)
+        {
+            if (id != question.ID)
+                return BadRequest();
+
+            context.Entry(question).State = EntityState.Modified;
+
+            await context.SaveChangesAsync();
+
+            return Ok(question);
         }
     }
 }
